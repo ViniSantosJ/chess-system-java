80d9da0e6047d5822ad230d96671270433c52abe
Vinicius Santos <vinicius.santosj95@gmail.com>
2022-12-04 03:54:26 -0300

    Starting to implement Board and Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,21 @@
+package boardgame;
+
+public class Piece {
+
+	protected Position position;
+	private Board board;
+
+	public Piece() {
+	}
+
+	public Piece(Board board) {
+		this.board = board;
+		this.position = null; // por padrão, já vem null. (opcional)
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+
+	//Não terá o set do Board, pois o tabulheiro não poderá ser alterado.
+}
