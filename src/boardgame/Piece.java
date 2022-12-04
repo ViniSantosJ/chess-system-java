5f06b1274f95dae4091d142d1ee0131cc0848c4d
Vinicius Santos <vinicius.santosj95@gmail.com>
2022-12-04 18:34:13 -0300

    Chess layer and printing the board

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -5,9 +5,6 @@ public class Piece {
 	protected Position position;
 	private Board board;
 
-	public Piece() {
-	}
-
 	public Piece(Board board) {
 		this.board = board;
 		this.position = null; // por padrão, já vem null. (opcional)
